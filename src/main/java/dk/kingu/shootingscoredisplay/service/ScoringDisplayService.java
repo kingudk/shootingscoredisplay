ddf999732026ab0e0166be45312a36183f7a8299
Kim Christensen <ktc@statsbiblioteket.dk>
2013-11-19 21:15:32 +0100

    clean up (remove) old code

--- src/main/java/dk/kingu/shootingscoredisplay/service/ScoringDisplayService.java
+++ src/main/java/dk/kingu/shootingscoredisplay/service/ScoringDisplayService.java
@@ -1,22 +1,16 @@
 package dk.kingu.shootingscoredisplay.service;
 
-import dk.kingu.shootingscoredisplay.datastore.DataStore;
-import dk.kingu.shootingscoredisplay.datastore.MemoryBasedDataStore;
-import dk.kingu.shootingscoredisplay.datastore.MemoryBasedDataStoreFactory;
 import dk.kingu.shootingscoredisplay.datastore.DIF.TournamentDAO;
 import dk.kingu.shootingscoredisplay.event.DBEventStorer;
-import dk.kingu.shootingscoredisplay.event.EventStorer;
 import dk.kingu.shootingscoredisplay.event.ScoreEventLogger;
 import dk.kingu.shootingscoredisplay.sius.SiusDataSource;
 
 public class ScoringDisplayService {
 
 	private SiusDataSource dataSource;
-	private DataStore dataStore;
 	private TournamentDAO dao;
 	
 	public ScoringDisplayService(TournamentDAO dao) {
-		dataStore = MemoryBasedDataStoreFactory.getDataStore();
 		//dataSource = new SiusDataSource("localhost", "4000");
 		//dataSource = new SiusDataSource("192.168.1.13", "4000");
 		dataSource = new SiusDataSource(ScoringDisplayServiceFactory.getSiusDataHost(), 
